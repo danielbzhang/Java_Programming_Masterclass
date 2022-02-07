9a9c636e959ed166f13ccf672e5915ff41e4d503
Daniel B. Zhang <42197094+danielbzhang@users.noreply.github.com>
2022-02-07 13:37:05 -0500

    Update decimalComparator.java

--- decimalComparator.java
+++ decimalComparator.java
@@ -16,15 +16,15 @@ public class Main {
 
     public static void main(String[] args) {
         boolean test1 = DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175);
-        System.out.println(test1);
+        System.out.println(test1); // TRUE
 
         boolean test2 = DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176);
-        System.out.println(test2);
+        System.out.println(test2); // FALSE
 
         boolean test3 = DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0);
-        System.out.println(test3);
+        System.out.println(test3); // TRUE
 
         boolean test4 = DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123);
-        System.out.println(test4);
+        System.out.println(test4); // FALSE
     }
 }
